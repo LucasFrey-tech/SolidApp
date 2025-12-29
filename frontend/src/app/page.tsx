40238eb8a79aea8bb3ac1d48520b60b6513e71fc
Nicolas <nicooperezz0@gmail.com>
2025-12-29 16:54:13 -0300

    Inicio-v1

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,95 +1,61 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import React from 'react';
+import Image from 'next/image';
+import styles from './page.module.css';
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className={styles.container}>
+    <section className={styles.banner}>
+    <Image
+      src="/banners/banner-inicio.png"
+      alt="Banner"
+      fill
+      className={styles.image}
+    />
+   <div className={styles.overlay}>
+    <h2 className={styles.titleBanner}>Un pequeño gesto, un gran impacto.</h2>
+    <button className={styles.buttonBanner}>Info aquí</button>
+  </div>
+</section>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+    <div className={styles.pasosDonar}>
+    <div className={styles.iconosDonar}>
+      <Image
+        src="/fotosInicio/donar.png"
+        alt="Icono 1"
+        width={80}
+        height={80}
+      />
+      <h3 className={styles.tituloIcono}>Doná</h3>
+      <p className={styles.textoIcono}>Seleccioná entre ropa, alimentos o juguetes.</p>
+    </div>
+    <div className={styles.iconosDonar}>
+      <Image
+        src="/fotosInicio/puntos.png"
+        alt="Icono 1"
+        width={80}
+        height={80}
+      />
+      <h3 className={styles.tituloIcono}>Obten puntos</h3>
+      <p className={styles.textoIcono}>Obtene puntos por donar artículos</p>
+    </div>
+    <div className={styles.iconosDonar}>
+      <Image
+        src="/fotosInicio/canjeo.png"
+        alt="Icono 1"
+        width={80}
+        height={80}
+      />
+      <h3 className={styles.tituloIcono}> Canjea por premios</h3>
+      <p className={styles.textoIcono}>Reclamá premios con tus puntos</p>
+    </div>
+
+
+
+
+    </div>
+
     </div>
   );
 }
