b3de3cafb2ddb0c3cde091e107e564b19a250e9b
LucasFrey-tech <lucasfrey19@gmail.com>
2026-01-03 16:14:40 -0300

    Feat: Crear el modulo de la App

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,64 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+import app_config from '../private/app.config.json';
+import { AppConfig } from '../private/app.config.interface';
+
+// Modulos
+import { UserModule } from './Modules/user/user.module';
+import { OrganizationModule } from './Modules/organization/organization.module';
+import { DonationModule } from './Modules/donation/donation.module';
+import { DonorModule } from './Modules/donor/donor.module';
+import { CompanyModule } from './Modules/company/company.module';
+import { CampaignModule } from './Modules/campaign/campaign.module';
+import { BenefitModule } from './Modules/benefit/benefit.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [() => app_config as AppConfig],
+    }),
+
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService<AppConfig>) => {
+        const db = configService.get('database_connection', { infer: true });
+
+        if (!db) {
+          throw new Error('Database configuration not found');
+        }
+
+        return {
+          type: 'mssql',
+          host: db.host,
+          port: db.port,
+          database: db.name,
+          entities: [__dirname + '/**/*.entity{.ts,.js}'],
+          synchronize: true,
+          options: {
+            encrypt: false,
+            trustServerCertificate: true,
+          },
+          extra: {
+            authentication: {
+              type: 'default',
+              options: {
+                trustedConnection: true,
+              },
+            },
+          },
+        };
+      },
+    }),
+    UserModule,
+    OrganizationModule,
+    DonationModule,
+    DonorModule,
+    CompanyModule,
+    CampaignModule,
+    BenefitModule,
+  ],
 })
 export class AppModule {}
