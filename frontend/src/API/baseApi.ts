8d0b6b51c10e3b18cd373a6eeb295e6f6e76ccfe
LucasFrey-tech <lucasfrey19@gmail.com>
2026-01-15 23:31:24 -0300

    fix: port number

--- frontend/src/API/baseApi.ts
+++ frontend/src/API/baseApi.ts
@@ -8,7 +8,7 @@ export class BaseApi {
     public readonly register: RegisterService;
 
     constructor(private token?:string){
-        const baseUrl = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000";
+        const baseUrl = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3001";
 
         this.log = new Login();
         this.register = new RegisterService(baseUrl);
