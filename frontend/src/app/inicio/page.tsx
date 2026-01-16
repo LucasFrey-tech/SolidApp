a004ed552e0ef9cad0949e1f14355e5ee0ba8128
LucasFrey-tech <lucasfrey19@gmail.com>
2026-01-16 13:51:30 -0300

    Fix: Corregir Route

--- frontend/src/app/inicio/page.tsx
+++ frontend/src/app/inicio/page.tsx
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import Image from 'next/image';
-import styles from './page.module.css';
+import styles from '@/styles/inicio.module.css';
 import Link from 'next/link'
 
 
