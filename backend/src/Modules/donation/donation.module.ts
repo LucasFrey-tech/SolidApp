92fea381b5c2f554ed1941a7d60b52c9fba0464d
Gaston Paez <104225501+GastonMPaez@users.noreply.github.com>
2026-01-23 17:33:05 -0300

    ranking3, la venganza del module

--- backend/src/Modules/donation/donation.module.ts
+++ backend/src/Modules/donation/donation.module.ts
@@ -6,9 +6,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Campaigns } from '../../Entities/campaigns.entity';
 import { Usuario } from '../../Entities/usuario.entity';
 import { RankingDonador } from '../../Entities/ranking.entity';
+import { RankingModule } from '../ranking/ranking.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Donations, Campaigns, Usuario, RankingDonador])],
+  imports: [TypeOrmModule.forFeature([Donations, Campaigns, Usuario, RankingDonador]), RankingModule],
   controllers: [DonationsController],
   providers: [DonationsService],
   exports: [DonationsService],
