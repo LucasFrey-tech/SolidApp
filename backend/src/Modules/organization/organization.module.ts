be99186aa320187d55a9528cc0717da397b3594a
LucasFrey-tech <lucasfrey19@gmail.com>
2026-02-02 19:47:13 -0300

    Fix: Agregar dependencia de modulo organizacion-campaña

--- backend/src/Modules/organization/organization.module.ts
+++ backend/src/Modules/organization/organization.module.ts
@@ -4,14 +4,14 @@ import { OrganizationsService } from './organization.service';
 import { Organizations } from '../../Entities/organizations.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { JwtModule } from '@nestjs/jwt';
-
+import { Campaigns } from '../../Entities/campaigns.entity';
 @Module({
   imports: [
     JwtModule.register({
       secret: process.env.JWT_SECRET || 'defaultSecret',
       signOptions: { expiresIn: '2h' },
     }),
-    TypeOrmModule.forFeature([Organizations]),
+    TypeOrmModule.forFeature([Organizations, Campaigns]),
   ],
   controllers: [OrganizationsController],
   providers: [OrganizationsService],
