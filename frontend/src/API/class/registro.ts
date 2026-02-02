9767a29e7b40bf19be5cd7c2882ed995cc6fef7e
LucasFrey-tech <lucasfrey19@gmail.com>
2026-02-02 19:51:32 -0300

    Chore: cambios varios

--- frontend/src/API/class/registro.ts
+++ frontend/src/API/class/registro.ts
@@ -1,34 +1,41 @@
-import { RegisterUsuarioRequest, RegisterEmpresaRequest, RegisterOrganizacionRequest, AuthResponse } from "@/API/types/register";
+import {
+  RegisterUsuarioRequest,
+  RegisterEmpresaRequest,
+  RegisterOrganizacionRequest,
+  AuthResponse,
+  Register,
+} from "@/API/types/register";
+import { Crud, PaginatedResponse } from "../service";
 
-export class RegisterService {
-  private baseUrl: string;
-
-  constructor(baseUrl?: string) {
-    this.baseUrl = baseUrl || process.env.NEXT_PUBLIC_API_URL || "http://localhost:3001";
+export class RegisterService extends Crud<Register> {
+  constructor() {
+    super()
     console.log("RegisterService baseUrl:", this.baseUrl);
     console.log("NEXT_PUBLIC_API_URL:", process.env.NEXT_PUBLIC_API_URL);
   }
 
   async registerUser(data: RegisterUsuarioRequest): Promise<AuthResponse> {
-    return this.post("/auth/register/usuario", data );
+    return this.post("/auth/register/usuario", data);
   }
 
   async registerEmpresa(data: RegisterEmpresaRequest): Promise<AuthResponse> {
-    return this.post("/auth/register/empresa", data );
+    return this.post("/auth/register/empresa", data);
   }
 
-  async registerOrganizacion(data: RegisterOrganizacionRequest): Promise<AuthResponse> {
-    return this.post("/auth/register/organizacion", data );
+  async registerOrganizacion(
+    data: RegisterOrganizacionRequest,
+  ): Promise<AuthResponse> {
+    return this.post("/auth/register/organizacion", data);
   }
 
   private async post<T>(path: string, data: unknown): Promise<T> {
     const url = `${this.baseUrl}${path}`;
     console.log("POST a:", url);
-    console.log("Con:", data); 
+    console.log("Con:", data);
 
     const res = await fetch(url, {
       method: "POST",
-      headers: { "Content-Type": "application/json" },
+      headers: this.getHeaders(), // Necesario el token al ser endpoint publico?
       body: JSON.stringify(data),
     });
 
@@ -37,5 +44,26 @@ export class RegisterService {
       throw new Error(`HTTP ${res.status}: ${errorText}`);
     }
     return res.json();
+  }
+
+  getAll(): Promise<Register[]> {
+    throw new Error("Method not implemented");
+  }
+
+  getAllPaginated(page?: number, limit?: number): Promise<PaginatedResponse<Register>> {
+    throw new Error("Method not implemented");
+  }
+
+  getOne(_id: number): Promise<Register> {
+    throw new Error("Method not implemented.");
+  }
+  create(_data: Partial<Register>): Promise<Register> {
+    throw new Error("Method not implemented.");
+  }
+  update(_id: number, data: Partial<Register>): Promise<Register> {
+    throw new Error("Method not implemented.");
+  }
+  delete(_id: number): Promise<void> {
+    throw new Error("Method not implemented.");
   }
 }
