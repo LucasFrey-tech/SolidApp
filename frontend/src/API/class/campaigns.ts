aaa0314fd4c668571e619e86b246dff986fd79cd
LucasFrey-tech <lucasfrey19@gmail.com>
2026-02-02 19:51:06 -0300

    Feat: agregar campaña al front api

---
+++ frontend/src/API/class/campaigns.ts
@@ -0,0 +1,91 @@
+import { Crud, PaginatedResponse } from "../service";
+import { Campaign, CampaignCreateRequest, CampaignUpdateRequest } from "../types/campañas/campaigns";
+
+export class campaignService extends Crud<Campaign> {
+  protected endPoint = "/campaigns";
+
+  constructor(token?: string) {
+    super(token);
+  }
+
+  async getAll(): Promise<Campaign[]> {
+    const res = await fetch(`${this.baseUrl}${this.endPoint}`, {
+        headers: this.getHeaders(),
+    });
+
+    if (!res.ok) {
+      throw new Error("Error al obtener campañas");
+    }
+
+    return res.json();
+  }
+
+  async getAllPaginated(
+    page: 1,
+    limit: 20,
+  ): Promise<PaginatedResponse<Campaign>> {
+    const res = await fetch(
+        `${this.baseUrl}${this.endPoint}?page=${page}&limit=${limit}`,
+        {
+            headers: this.getHeaders(),
+        },
+    );
+
+    if (!res.ok) {
+      throw new Error("Error al obtener organizaciones paginadas");
+    }
+
+    return res.json();
+  }
+
+  async getOne(id: number): Promise<Campaign> {
+    const res = await fetch(`${this.baseUrl}${this.endPoint}/${id}`, {
+        headers: this.getHeaders(),
+    });
+
+    if(!res.ok) {
+        throw new Error("Campaña no encontrada");
+    }
+
+    return res.json();
+  }
+  
+  async create(data: CampaignCreateRequest): Promise<Campaign> {
+    const res = await fetch(`${this.baseUrl}${this.endPoint}`, {
+        method: "POST",
+        headers: this.getHeaders(),
+        body: JSON.stringify(data),
+    });
+
+    if (!res.ok) {
+        throw new Error("Error al crear campaña");
+    }
+
+    return res.json();
+  }
+
+  async update(id: number, data: CampaignUpdateRequest): Promise<Campaign> {
+    const res = await fetch(`${this.baseUrl}${this.endPoint}/${id}`, {
+       method: "PUT",
+       headers: this.getHeaders(),
+       body: JSON.stringify(data), 
+    });
+
+    if (!res.ok) {
+        throw new Error("Error al actualizar campaña");
+    }
+
+    return res.json();
+  }
+
+  async delete(id: number): Promise<void> {
+    const res = await fetch(`${this.baseUrl}${this.endPoint}/${id}`, {
+        method: "DELETE",
+        headers: this.getHeaders(),
+    });
+    
+    if (!res.ok) {
+      throw new Error("Error al eliminar la campaña");
+    }
+  }
+}
