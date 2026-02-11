413f92aa655ddb54a69c063c8fcfcfdc584e7624
LucasFrey-tech <lucasfrey19@gmail.com>
2026-02-11 20:02:53 -0300

    Fix: agregar imagenes de campañas al modulo campaña

--- backend/src/Modules/campaign/campaign.module.ts
+++ backend/src/Modules/campaign/campaign.module.ts
@@ -4,6 +4,7 @@ import { CampaignsService } from './campaign.service';
 import { Campaigns } from '../../Entities/campaigns.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Organizations } from '../../Entities/organizations.entity';
+import { Campaigns_images } from '../../Entities/campaigns_images.entity';
 
 /**
  * Módulo de NestJS que agrupa los componentes realcionados a Campañas:
@@ -12,7 +13,9 @@ import { Organizations } from '../../Entities/organizations.entity';
  * - Repositorios TypeORM
  */
 @Module({
-  imports: [TypeOrmModule.forFeature([Campaigns, Organizations])],
+  imports: [
+    TypeOrmModule.forFeature([Campaigns, Organizations, Campaigns_images]),
+  ],
   controllers: [CampaignsController],
   providers: [CampaignsService],
   exports: [CampaignsService],
