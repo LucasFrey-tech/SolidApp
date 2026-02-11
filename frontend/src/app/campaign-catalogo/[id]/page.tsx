238a6e93d971e7863c8e3b0f2c257783600244bb
LucasFrey-tech <lucasfrey19@gmail.com>
2026-02-11 18:55:27 -0300

    Fix: Renombrar carpeta

--- frontend/src/app/campaign-catalogo/[id]/page.tsx
+++ frontend/src/app/campaign-catalogo/[id]/page.tsx
@@ -2,35 +2,6 @@ import { notFound } from "next/navigation";
 import Link from "next/link";
 import styles from "@/styles/donarDetalle.module.css";
 
-const mockCauses = [
-  {
-    id: 1,
-    title: "Comedor Comunitario",
-    description: "Ayudá a que más familias puedan acceder a una comida diaria.",
-    image: "https://images.unsplash.com/photo-1600891964599-f61ba0e24092",
-    content:
-      "Este comedor brinda desayuno, almuerzo y merienda todos los días a familias que lo necesitan.",
-  },
-  {
-    id: 2,
-    title: "Refugio de Animales",
-    description:
-      "Donaciones destinadas a rescate, alimento y atención veterinaria.",
-    image: "https://images.unsplash.com/photo-1548199973-03cce0bbc87b",
-    content:
-      "El refugio cuida y rehabilita animales abandonados y heridos.",
-  },
-  {
-    id: 3,
-    title: "Educación Solidaria",
-    description:
-      "Apoyá a estudiantes con útiles, becas y materiales educativos.",
-    image: "https://images.unsplash.com/photo-1588072432836-e10032774350",
-    content:
-      "Ayudamos a que más chicos puedan seguir estudiando y formándose.",
-  },
-];
-
 export default function DonacionDetallePage({
   params,
 }: {
@@ -64,7 +35,7 @@ export default function DonacionDetallePage({
 
         {/* VOLVER */}
         <Link
-          href="/donaciones-catalogo"
+          href="/campaign-catalogo"
           className={styles.backButton}
         >
           ← Volver al catálogo
