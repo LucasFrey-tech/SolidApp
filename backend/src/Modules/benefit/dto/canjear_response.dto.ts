9e071c5c579ba008a40041f6a31ef46fbc9f293e
LucasFrey-tech <lucasfrey19@gmail.com>
2026-04-13 13:58:10 -0300

    chore: agregar doc swagger a canjearResponseDto

--- backend/src/Modules/benefit/dto/canjear_response.dto.ts
+++ backend/src/Modules/benefit/dto/canjear_response.dto.ts
@@ -1,7 +1,21 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CanjearResponseDto {
+  @ApiProperty({ example: true, description: 'Indica si el canje fue exitoso' })
   success: boolean;
+
+  @ApiProperty({ example: 2, description: 'Cantidad de unidades canjeadas' })
   cantidadCanjeada: number;
+
+  @ApiProperty({ example: 100, description: 'Puntos gastados en el canje' })
   puntosGastados: number;
+
+  @ApiProperty({
+    example: 50,
+    description: 'Puntos restantes del usuario después del canje',
+  })
   puntosRestantes: number;
+
+  @ApiProperty({ example: 98, description: 'Stock restante del beneficio' })
   stockRestante: number;
 }
